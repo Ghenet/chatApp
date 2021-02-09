ad0efad7d47b14e63188716b0347b6a86e7d6f62
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-09 12:52:45 -0800

    gets the last key-Index if there is & checks the userName

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,24 @@
+import { ConsoleSqlOutlined } from '@ant-design/icons';
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessge';
+
 const ChatFeed = (props) => {
-console.log(props);
+    const { chats, activeChat, userName, messages } = props;
+
+    const chat = chats && chats[activeChat];
+    
+    const renderMessages = () => {
+        const keys = Object.keys(messages);
+        console.log(keys);
+
+        // return keys.map((key,index) => {
+        //     const message = messages[key];
+
+        // })
+    }
+
+    renderMessages();
 
 return (
     <div>
---
+++ src/components/TheirMessge.jsx
@@ -0,0 +1,9 @@
+const TheidMessage = () => {
+    return (
+        <div>
+             Their Message
+        </div>
+    );
+}
+
+export default TheidMessage;
