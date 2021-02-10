01be0f8e72a44d43e023be7bdf3af3300b4b5b9c
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-09 19:08:53 -0800

    renders the img if the msg is an img

--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,4 +1,14 @@
-const MyMessage = () => {
+const MyMessage = ({ message }) => {
+    if(message?.attachements?.length > 0) {
+        return (
+            <img 
+            src={message.attachements[0].file}
+            alt="message-attahcement"
+            className="message-image"
+            style={{ float:'right' }}
+            />
+        )
+    }
     return (
         <div>
              My Message
