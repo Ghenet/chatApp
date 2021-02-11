9597174499582ac352e795b5e5b7c94110034217
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-11 13:23:51 -0800

    adds the handleChange func

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
-const MessageForm = () => {
+const MessageForm = (props) => {
     const [value, setValue] = useState('');
+    const { chatId, creds } = props;
 
     const handleSubmit = () => {
 
     }
-    const handleChange = () => {
+    const handleChange = (event) => {
+        setValue(event.target.value);
 
+        isTyping(props,chatId);
     }
     return (
       <form action="" className="message-form" onSubmit={handleSubmit}>
