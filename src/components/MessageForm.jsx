4720dbc2dfb8f06492f4ba5c5e74299c616448c6
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-11 13:33:10 -0800

    adds the image field

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
     const [value, setValue] = useState('');
@@ -28,6 +29,11 @@ const MessageForm = (props) => {
           onChange={handleChange}
           onSubmit={handleSubmit}
           />
+          <label htmlFor="upload-button">
+              <span className="image-button">
+                  <PictureOutlined className="picture-icon" />
+              </span>
+          </label>
       </form>
     );
 }
