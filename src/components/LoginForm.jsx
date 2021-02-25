2e19538d7c991f3c0c6627ff123738b19b85176e
Ghenet Andemiachael <ghenikibrom@gmail.com>
2021-02-24 17:26:35 -0800

    renders the LoginForm if not logged in

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,7 +2,7 @@ import { useState }from 'react';
 import axios from 'axios';
 
 const LoginForm = () => {
-    const [username, setUsername] = uUeState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit =  async (e) => {
@@ -28,7 +28,7 @@ const LoginForm = () => {
                 <h1 className="title">Chat Applications</h1>
                 <form onSubmit={handleSubmit}>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input"  placeholder="Username" required />
-                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} classNmae="input" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
                     <div align="center">
                         <button type="submit" className="button">
                             <span>Start Chatting</span>
@@ -43,4 +43,4 @@ const LoginForm = () => {
     )
 }
 
-export default LoginForm();
+export default LoginForm;
